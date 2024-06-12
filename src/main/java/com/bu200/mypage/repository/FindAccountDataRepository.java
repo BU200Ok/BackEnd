0baa086ce9624d6f18b1a084b09162ba97f8df96
chungjeongsu <143079383+chungjeongsu@users.noreply.github.com>
2024-06-12 17:36:00 +0900

    fix: forum 내 정보 일부 문제 수정

--- src/main/java/com/bu200/mypage/repository/FindAccountDataRepository.java
+++ src/main/java/com/bu200/mypage/repository/FindAccountDataRepository.java
@@ -1,15 +1,24 @@
 package com.bu200.mypage.repository;
 
 import com.bu200.login.entity.Account;
+import com.bu200.login.entity.Attendance;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FindAccountDataRepository extends JpaRepository<Account, Long> {
-    Account findByAccountId(String accountId);  //find
-    //TodoList 컴포넌트
-    //TodoList findTodoListByTodoListCode(Long todoListCode);
+    //account, project, attendance, team, department
+    @Query("select a from Account a " +
+            "left join fetch a.projects p " +
+            "left join fetch a.team t " +
+            "left join fetch t.department d " +
+            "where a.accountId = :accountId")
+    Account findAccountWithHighPriorityProject(@Param("accountId") String accountId);
 
 }
